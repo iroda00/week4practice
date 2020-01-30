51efcc311a7f82d88e1b139a88388f5ab5d9e88d
iroda999 <60248444+iroda00@users.noreply.github.com>
2020-01-29 20:54:27 -0500

    some new things added

---
+++ functions.py
@@ -0,0 +1,45 @@
+# # Functions
+# # defining the function
+def greet_user():
+     """Function to greet the user"""
+     print("Hello")
+# # calling the function
+greet_user()
+
+
+# # # comment 
+
+
+# # # passing the arguments to our function
+def greet_user_arg(name, lastname)
+    """Function to greet user with name."""
+# #     return(f'Hello, {name},{lastname}')
+
+
+# # msg = greet_user_arg('John', 'Lee')
+# # print(msg)
+
+# # find tiangle area
+# # def triangle_area(height=0, base=0):
+# #     return height * base * 0.5
+
+# # print(triangle_area(4, 5))
+# def triangle_area(b, h):
+#     """returns the area of a triangle with base b and height h."""
+#     return 0.5 * b * h
+
+# triangle_area(3, 6)
+
+
+# # print(triangle_area(4, 5))
+
+# import math
+# dir(math)
+# math.pi
+# def volume(r):
+#     """Returns the volume of a sphere with radius r."""
+#     v = (4.0/3.0)* math.pi * r**3
+#     return v
+
+# volume(2)
+
